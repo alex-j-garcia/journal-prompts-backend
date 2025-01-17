16558457e168f56c183a4767407b1bfe7e94a9c4
Alex J Garcia <alexshuu@gmail.com>
2025-01-16 20:58:12 -0500

    Add route for getting prompt by ID

--- controllers/prompts.js
+++ controllers/prompts.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
 const Prompt = require('../models/prompt')
 
-router.get('/', async (request, response) => {
-  const prompts = await Prompt.find({});
-  return response.json(prompts);
+router.get('/', async (request, response, next) => {
+  try {
+    const prompts = await Prompt.find({});
+    response.json(prompts);
+  } catch(exception) {
+    next(exception);
+  }
+});
+
+router.get('/:id', async (request, response, next) => {
+  try {
+    const prompt = await Prompt.findById(request.params.id);
+    response.json(prompt);
+  } catch(exception) {
+    next(exception);
+  }
 });
 
 module.exports = router;
