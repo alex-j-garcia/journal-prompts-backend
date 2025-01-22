ac17610d4df009b63797f5e0f8e60e4b7ff15f49
Alex J Garcia <alexshuu@gmail.com>
2025-01-22 11:08:11 -0500

    Refactor database utitlity and extract prompts
    Extract all prompts into JSON file
    
    Refactor database utility

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Prompt = require('../models/prompt');
+const prompts = require('./prompts.json');
+
 require('dotenv').config();
 
 mongoose.set('strictQuery', false);
@@ -11,57 +13,19 @@ mongoose.connect(process.env.MONGODB_URI).then((promise) => {
   console.log(`An error occurred: ${error.message}`);
 });
 
-const prompts = [
-  {
-    "content": "In 5 years' time, what would your ideal life look like? What would the worst version look like?",
-    "tag": "goals",
-  },
-  {
-    "content": "Who are you most grateful for and why?",
-    "tag": "relationships",
-  },
-  {
-    "content": "Is there a difficult conversation you're avoiding? If so, why?",
-    "tag": "relationships",
-  },
-  {
-    "content": "What favorite childhood activity do you no longer engage in?",
-    "tag": "personality",
-  },
-  {
-    "content": "If money wasn't an obstacle, what would you do for work?",
-    "tag": "personality",
-  },
-  {
-    "content": "What makes you excited to get out of bed in the morning",
-    "tag": "goals",
-  },
-  {
-    "content": "Is there a habit that's holding you back from reaching your full potential?",
-    "tag": "personality",
-  },
-  {
-    "content": "What could you do to make today just 1% better than yesterday?",
-    "tag": "goals",
-  },
-  {
-    "content": "What's a piece of advice you would give to your younger self?",
-    "tag": "personality",
-  },
-  {
-    "content": "What would you like to be remembered for when you're gone?",
-    "tag": "goals",
-  },
-];
+const setDatabasePrompts = async () => {
+  await Prompt.deleteMany({});
 
-const promptsPromises = prompts.map((p) => {
-  const prompt = new Prompt({ ...p, activePrompt: false, answers: [] });
-  return prompt.save();
-});
+  const promptPromises = prompts.map((p) => {
+    const prompt = new Prompt({ ...p, activePrompt: false, answers: [] });
+    return prompt.save();
+  });
 
-Promise.all(promptsPromises).then((promise) => {
-  console.log('asynchronous code completed, all items should be saved');
-  mongoose.connection.close();
-});
+  await Promise.all(promptPromises);
+  console.log('Database reset and new prompts saved');
 
-console.log('Synchronous code completed, waiting for saving...')
+  await mongoose.connection.close();
+  console.log('Connection to MongoDB closed');
+};
+
+setDatabasePrompts();
