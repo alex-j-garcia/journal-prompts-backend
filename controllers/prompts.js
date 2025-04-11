4380f9bc158f2a191d72f51cddeeff2d11f04686
Alex J Garcia <alexshuu@gmail.com>
2025-04-10 20:21:31 -0400

    refactor: update formatting

--- controllers/prompts.js
+++ controllers/prompts.js
@@ -8,23 +8,28 @@ promptsRouter.get('/', async (request, response, next) => {
     
     const answer = await Answer.findOne({
       user: request.body.user,
-      promptId: activePrompt._id
+      promptId: activePrompt._id,
     });
  
     if (!answer) {
-      return response.status(200).json({ 
-        ...activePrompt.toJSON(),
-        answers: []
-      });
+      return response
+        .status(200)
+        .json({ 
+          ...activePrompt.toJSON(),
+          answers: [],
+        });
     }
 
     const answers = await Answer
-      .find({
-        promptId: activePrompt._id.toString(),
-      })
+      .find({ promptId: activePrompt._id.toString() })
       .populate('user', { username: 1 });
     
-    response.json({ ...activePrompt.toJSON(), answers, });
+    response
+      .status(200)
+      .json({
+        ...activePrompt.toJSON(),
+        answers,
+      });
   } catch(exception) {
     next(exception);
   }
