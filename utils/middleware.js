e904b47983a35106c60bef16737c7fd367274ff5
Alex J Garcia <alexshuu@gmail.com>
2026-01-21 06:02:02 -0500

    feat(middleware): catch MongooseError

--- utils/middleware.js
+++ utils/middleware.js
@@ -8,13 +8,19 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-const getTokenFromRequest = (request, response, next) => {
-  const userTokenOrNull = request.get('authorization');
+/**
+ * Enriches the request body with the user's ID if a valid
+ * bearer token exists.
+ */
+const getUserIdFromToken = (request, response, next) => {
+  const authorizationHeader = request.headers.authorization;
+  const hasAuthHeader = authorizationHeader
+    && authorizationHeader.startsWith('Bearer ');
   
-  if (userTokenOrNull && userTokenOrNull.startsWith('Bearer ')) {
-    const token = userTokenOrNull.replace('Bearer ', '');
+  if (hasAuthHeader) {
+    const token = authorizationHeader.replace('Bearer ', '');
     const decodedToken = jwt.verify(token, process.env.SECRET);
-    request.body.user = decodedToken ? decodedToken.id : null;
+    request.body.userId = decodedToken ? decodedToken.id : null;
   }
 
   next();
@@ -22,22 +28,25 @@ const getTokenFromRequest = (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformed ID' });
+    return response.status(400).send({ message: 'malformed ID' });
   } else if (error.name === 'ValidationError'
       && error.message.includes('is shorter than the minimum allowed length (100).')) {
-    return response.status(400).send({ error: 'answer is too short' });
+    return response.status(400).send({ message: 'answer is too short' });
   } else if (error.name === 'ValidationError'
       && error.message.includes('is longer than the maximum allowed length (1500).')) {
-    return response.status(400).send({ error: 'answer is too long' });
+    return response.status(400).send({ message: 'answer is too long' });
   } else if (error.name === 'ValidationError'
       && error.message.includes('answer: Path `answer` is required.')) {
-    return response.status(400).send({ error: 'answer is required' });
+    return response.status(400).send({ message: 'answer is required' });
   } else if (error.name === 'ValidationError'
       && error.message.includes('username: Path `username` is required.')) {
-    return response.status(400).send({ error: 'property "username" is required' });
+    return response.status(400).send({ message: 'property "username" is required' });
   } else if (error.name === 'MongoServerError'
       && error.message.includes('E11000 duplicate key error')) {
-    return response.status(400).send({ error: 'this username is already taken.' });
+    return response.status(400).send({ message: 'this username is already taken.' });
+  } else if (error.name === 'MongooseError'
+      && error.message.includes('buffering timed out after 10000ms')) {
+    return response.status(400).send({ message: 'Unable to fetch prompt. Check internet connection and try again.' });
   }
   
   next(error);
@@ -49,7 +58,7 @@ const unknownEndpoint = (request, response) => {
 
 export default {
   requestLogger,
-  getTokenFromRequest,
+  getUserIdFromToken,
   unknownEndpoint,
   errorHandler,
 };
